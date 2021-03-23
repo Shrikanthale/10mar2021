77f11bfa3ff6c96021f8dfd61b285822b3324767
shrikant <skanthale9@gmail.com>
2021-03-23 08:36:20 -0700

    adding some concept of arrowfunction in js

---
+++ arrowfunction.js
@@ -0,0 +1,90 @@
+/*function hi(message){
+    console.log(message)
+    return "hi"
+}
+let mess = hi("world")
+console.log(mess)
+*/
+
+/*
+function add(){
+    let a = 10 
+    let b = 20 
+    let c = a+b
+    
+    return c
+}
+var result = add()
+console.log("the addition of two number is: " +result)
+*/
+
+/*
+hi = (message) => {console.log(message+" shri")}
+hi("hiiii")
+*/
+
+/*
+add = (a,b) => {
+      let c = a + b
+      return c
+}
+let result = add(50,35)
+console.log("result will be " +result)
+*/
+
+/*
+num = (a) => {
+     for(let i = 1 ; i<=10 ; i++){
+         var table = a*i;
+        console.log(table)
+     }
+     return table
+}
+var t= num(5)
+console.log(t)
+*/
+
+/*
+number = (s,x) => {
+    for(let i = 1 ; i<=10 ;i++){
+        var table = s * i;
+        var table1 = x * i
+        console.log(table)
+        console.log("                " +table1)
+    }
+    return table
+    return table1
+}
+var res = number(15,18)
+console.log(res)
+*/
+
+/*
+factor = ( f ) => {
+    var fact = 1
+    for( i=1 ; i<= f ; i++){
+         fact = i * fact
+        console.log(fact)
+    }
+    return fact
+}
+ factor(5)
+*/
+
+/*
+cube = (c) => {
+       var result = c*c*c
+       return result
+}
+var result = cube(4)
+console.log("cube of number is " + result)
+*/
+/*
+areaofcircle = (r) => {
+    var pi = 3.14
+    var result = pi*r*r
+    return result
+}
+result = areaofcircle(34)
+console.log("area of circle is : " +result)
+*/
---
+++ cpcv.js
@@ -0,0 +1,27 @@
+// copy by value and copy by reference
+// using function as a constructor
+//class in js
+// object destrustingy
+
+class mobile {
+    constructor(size,name,color,mic){
+        this.size=size
+        this.name=name
+        this.color=color
+        this.mic=mic
+    }
+    details(){
+        console.log(this.size)
+        console.log(this.name)
+        console.log(this.color)
+        console.log(this.mic)
+    }
+}
+getcall(caliingFunc){
+    return this.size+" "+color
+}
+}
+
+var mobile1= new mobile (6,"samsung","black","yes")
+console.log(mobile.name)
+
