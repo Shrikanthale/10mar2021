410af512d8c8269f985b77675ae0d7e4f8bdbb51
shrikant <skanthale9@gmail.com>
2021-03-24 06:18:32 -0700

    adding function object in js

---
+++ funcobj.js
@@ -0,0 +1,120 @@
+/*var employee = {
+    Name : "naren",
+    IDno : 456258,
+    work : "programmer",
+  
+
+    get : function(){
+        console.log(this.work)
+    }
+};
+
+employee.get()
+*/
+
+/*
+var Hero = {
+    Name : "AK",
+    work : "villen",
+    Mno : 785496235,
+    paid : 750000,
+
+    resume : function(){
+        console.log(this.paid)
+        console.log(this.Name)
+        console.log(this.Mno)
+    }
+};
+Hero.resume()
+*/
+
+/*
+var student = {
+    Name : "navin",
+    Idno : 458796,
+    course : "cs & it",
+    fees : 52000,
+
+    Deatil : function(){
+        console.log(this.Name);
+        console.log(this.Idno)
+        console.log(this.fees)
+    },
+    getInformation : function(LastName){
+    return this.Name + " " + LastName
+    }
+};
+console.log(student.getInformation("patak"))
+*/
+
+/*
+var employee = {
+    Name : "ankit",
+    Name1 : "rahul",
+    
+   
+  //  Deatil : function(){
+    //    console.log(this.Name)
+   // },
+
+    Getinfo : function(LastName){
+        return this.Name + " " + LastName
+        return this.Name1 + " " + LastName  
+    }
+};
+console.log(employee.Getinfo("sharma"))
+console.log(employee.Getinfo("rathod"))
+*/
+
+/*
+var girls = {
+    Name : "rani",
+
+    getinformation : function(LastName){
+        return this.Name + " " + LastName
+    }
+};
+console.log(girls.getinformation("awasati"))
+*/
+
+/*
+var boys = {
+    Name : "rakesh",
+
+    getinfo : function(Lastname){
+        return this.Name +"  " + Lastname
+    }
+};
+
+console.log(boys.getinfo("sharma"))
+*/
+
+/*
+var person = {
+    Name : "niramal",
+    id : 4589654,
+    salary : 75000,
+
+    deatil : function(){
+        console.log(this.id)
+        console.log(this.Name)
+    },
+  // getinformation : function(LastName){
+    //   return this.Name + "  " +LastName
+  // }   
+};
+//console.log(person.getinformation("rathod"))
+person.deatil()
+*/
+
+var train = {
+    Id : 4521563,
+    time : 10,
+    upto : "mumbai To goa",
+    deatil : function(){
+        console.log(this.Id)
+        console.log(this.time)
+        console.log(this.upto)
+    }
+}
+train.deatil()
