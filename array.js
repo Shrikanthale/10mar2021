60783610d1a56e9260158cf6c435520ba8b1e686
shrikant <skanthale9@gmail.com>
2021-03-25 00:38:57 -0700

    discuss about array and array function

---
+++ array.js
@@ -0,0 +1,113 @@
+/*var arr1 = [45,52,86,35,26]
+console.log(arr1[2])
+console.log(arr1[0]+arr1[1])
+console.log(arr1.length)
+console.log(arr1[4])
+var index = arr1.indexOf(86)
+console.log(index)
+*/
+
+/*
+var arr1 = [45,52,86,35,26]
+var add = arr1[0]
+for (let i= 0; i < arr1.length; i++) {
+         add = add + arr1[i];
+    }
+console.log("addition of array is : " +add)
+*/
+
+/*
+var arr = [5,8,3,1,6,9,4]
+var mul = arr[0]
+for(let i = 0 ; i<arr.length ; i++){
+    mul=mul*arr[i]
+}
+console.log(mul)
+*/
+
+/*
+var Name = ["s","h","r","i"]
+var inti = Name[0]
+for(let i = 0 ; i<Name.length ; i++){
+   
+    inti = inti + Name[0]
+    
+}
+console.log(inti)
+*/
+
+/*
+var arr2 = [23,12,78,10,62,43,22]
+var max = arr2[0]
+for(let i = 0 ; i < arr2.length ; i++){
+    if(max<=arr2[i]){
+        max=arr2[i]
+    }
+}
+console.log(max)
+*/
+
+/*
+var Numbers = [45,52,15,75,95,62,56,35]
+var max = Numbers[0]
+for(let i = 0 ; i < Numbers.length ; i++){
+    if(Numbers[i]>=max){
+         max=Numbers[i]
+    }
+}
+console.log("the max numbers in this array is : " +max)
+*/
+
+/*
+var numbers = [75,45,95,85,26,15,86,35]
+var min = numbers[0]
+for(let i = 0; i<numbers.length; i++){
+    if(min>=numbers[i]){
+        min = numbers[i]
+    }
+}
+console.log(min)
+*/
+
+/*
+var count = [35,95,62,45,75,15]
+var search = count.indexOf(45)
+
+    if(count[search]){
+        console.log("yes..... its found that number " + count[search] )
+    }
+    else {
+        console.log("error")
+    }
+*/
+
+/*
+var found = [23,20,15,52,21,56]
+var search = found.indexOf(15)
+if(found[search]){
+    console.log("yes its found "+ found[search] + "  " + search)
+}
+else{
+    console.log("error")
+}
+*/
+
+/*
+var char = ["s","h","r","i","k","a","n","t"]
+var find = char.indexOf("r")
+if(char[find]){
+    console.log("yes its found in array list " + char[find] + " " + find)
+}
+else{
+    console.log("error")
+}
+*/
+
+var digit = [45,58,62,35,15]
+var max = digit[0]
+for(let i = 0; i<digit.length; i++){
+    if(max<=digit[i]){
+        max = digit[i]
+    }
+}
+console.log(max)
