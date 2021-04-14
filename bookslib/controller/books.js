cfc0faf2a87a947d8ced4663c1010c1b895b7dc1
shrikant <skanthale9@gmail.com>
2021-04-14 14:38:48 -0400

    in controller connecting to db

--- bookslib/controller/books.js
+++ bookslib/controller/books.js
@@ -1,11 +1,55 @@
+import {Book} from '../models/book.js'
 let books = []
 
 export const getbook = (req,res) => {
     console.log("in getbook")
-    res.send(books)
+    //res.send(books)
+    Book.find()
+    .then(
+        (result)=>{
+            res.send(result)
+        }
+    )
+    .catch(
+        (err)=>{
+            console.log(err)
+        }
+    )
+}
+
+export const getbookById = (req,res) => {
+    console.log("on getbookbyid")
+    Book.findById(req.params.id)
+    .then(
+        (result) => {
+            res.send(result)
+        }
+    )
+    .catch(
+        (err) => {
+            console.log(err)
+        }
+    )
 }
 
 export const createbook = (req,res) =>{
     console.log("in creatuser")
-    res.send("ok")
+    //books.push(req.body)
+    const book = new Book({
+        author:req.body.author,
+        bookname:req.body.bookname,
+        available:req.body.available
+    })
+    //res.send("ok")
+    book.save()
+    .then(
+        (result) => {
+            res.send(result)
+        }
+    )
+    .catch(
+        (err) => {
+            res.send(err)
+        }
+    )
 }
