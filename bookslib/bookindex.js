b1ca67bef07d8d953eccf0021f58260b637402c4
shrikant <skanthale9@gmail.com>
2021-04-14 14:34:08 -0400

    create apis using mongoos

--- bookslib/bookindex.js
+++ bookslib/bookindex.js
@@ -1,9 +1,27 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import bookrouter from './routes/book.js'
+import mongoose from 'mongoose'
+
+var dbURI ='mongodb+srv://kanthale:kanthale1@cluster0.pcywb.mongodb.net/library?retryWrites=true&w=majority'
+mongoose.connect(dbURI,{useNewUrlParser:true,useUnifiedTopology:true})
+
+.then(
+    (result) => {
+        console.log("connecting to db....")
+        //console.log(result)
+        server.listen(PORT)
+        console.log("server got started sucessfully")
+    }
+)
+.catch(
+    (err)=>{
+        console.log(err)
+    }
+)
 
 const server = express()
-const PORT = 9001
+const PORT = 9002
 
 server.use(bodyParser.json())
 
@@ -12,4 +30,5 @@ res.send("welcome to book stall")
 server.use("/book",bookrouter)
 server.get("/", frontpage)
 
-server.listen(PORT)
+
+//mongodb+srv://kanthale:<password>@cluster0.pcywb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
