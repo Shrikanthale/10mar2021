711a15292caecf1b22163c12ca7dac659050ad1d
shrikant <skanthale9@gmail.com>
2021-04-14 14:39:51 -0400

    add getbook,createbook,getbookbtid

--- bookslib/routes/book.js
+++ bookslib/routes/book.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import {getbook,createbook} from '../controller/books.js'
+import {getbook,createbook,getbookById} from '../controller/books.js'
 const booker = express.Router()
 
 booker.get("/",getbook)
+booker.get("/:id",getbookById)
 booker.post("/",createbook)
+
 
 export default booker
