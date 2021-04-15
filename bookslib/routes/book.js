78b1639eaf6a7b8f2bb2371870ac890406ab2a01
shrikant <skanthale9@gmail.com>
2021-04-15 09:59:27 -0400

    add getbook,createbook,getbookbyid,updatebookbyid,deletebookbyid

--- bookslib/routes/book.js
+++ bookslib/routes/book.js
@@ -1,10 +1,10 @@
 import express from 'express'
-import {getbook,createbook,getbookById} from '../controller/books.js'
+import {getbook,createbook,getbookById,deletebookById,updatebookById} from '../controller/books.js'
 const booker = express.Router()
 
 booker.get("/",getbook)
 booker.get("/:id",getbookById)
 booker.post("/",createbook)
-
-
+booker.delete("/:id",deletebookById)
+booker.patch("/:id",updatebookById)
 export default booker
